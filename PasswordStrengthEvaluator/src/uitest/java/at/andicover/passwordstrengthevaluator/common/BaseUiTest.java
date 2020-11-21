118e173ea87091f7d3087cd46980f27d1516668b
AndiCover <a.schoengruber@gmx.net>
2020-11-21 15:11:39 +0100

    Added UI test base

---
+++ PasswordStrengthEvaluator/src/uitest/java/at/andicover/passwordstrengthevaluator/common/BaseUiTest.java
@@ -0,0 +1,46 @@
+package at.andicover.passwordstrengthevaluator.common;
+
+import at.andicover.passwordstrengthevaluator.PasswordStrengthEvaluatorWebApplication;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class BaseUiTest {
+
+    protected static WebDriver driver;
+    private static ConfigurableApplicationContext context;
+
+    @BeforeClass
+    public static void setup() {
+        startApplication();
+        initWebDriver();
+        navigateToLocalWebApplication();
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        driver.quit();
+        stopApplication();
+    }
+
+    private static void initWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    private static void startApplication() {
+        context = SpringApplication.run(PasswordStrengthEvaluatorWebApplication.class);
+    }
+
+    private static void stopApplication() {
+        SpringApplication.exit(context);
+    }
+
+    protected static void navigateToLocalWebApplication() {
+        driver.get("http://127.0.0.1:8080");
+    }
+}
---
+++ PasswordStrengthEvaluator/src/uitest/java/at/andicover/passwordstrengthevaluator/ui/MainViewUiTest.java
@@ -0,0 +1,12 @@
+package at.andicover.passwordstrengthevaluator.ui;
+
+import at.andicover.passwordstrengthevaluator.common.BaseUiTest;
+import org.junit.Test;
+
+public class MainViewUiTest extends BaseUiTest {
+
+    @Test
+    public void test() {
+        //TODO
+    }
+}
