70104f2bf61c94cef89d8df5d51505b8393482e6
AndiCover <a.schoengruber@gmx.net>
2020-11-23 17:52:56 +0100

    Read database settings from properties file

--- PasswordStrengthEvaluator/src/main/java/at/andicover/passwordstrengthevaluator/db/CassandraConnector.java
+++ PasswordStrengthEvaluator/src/main/java/at/andicover/passwordstrengthevaluator/db/CassandraConnector.java
@@ -9,29 +9,38 @@ import com.datastax.driver.core.schemabuilder.SchemaBuilder;
 import com.datastax.driver.core.schemabuilder.SchemaStatement;
 import com.google.common.collect.ImmutableMap;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Connection class for Cassandra DB.
  */
 public final class CassandraConnector {
 
-    private static final String NODE = System.getProperty("cassandra_node", "127.0.0.1");
     private static final String DEFAULT_WEAK_PASSWORD_FILE = "weak_passwords.txt";
-    private static final int PORT = 9042;
-    private static final String KEYSPACE = "test";
+    private static final String APPLICATION_PROPERTIES_FILE = "application.properties";
     private static final int REPLICATION_FACTOR = 3;
 
     private Cluster cluster;
     private Session session;
+    private Properties properties;
 
     /**
      * Opens the DB connection and initializes the DB (creating keyspace, tables, ...).
      */
     private void connect() {
+        try (InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(APPLICATION_PROPERTIES_FILE)) {
+            properties = new Properties();
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         Cluster.Builder builder = Cluster.builder()
-                .addContactPoint(NODE)
-                .withPort(PORT);
+                .addContactPoint(properties.getProperty("spring.data.cassandra.contact-points", "127.0.0.1"))
+                .withPort(Integer.parseInt(properties.getProperty("spring.data.cassandra.port", "9042")));
         cluster = builder
                 .withoutJMXReporting()
                 .build();
@@ -71,12 +80,13 @@ public final class CassandraConnector {
     }
 
     private void createKeyspace() {
-        final SchemaStatement statement = SchemaBuilder.createKeyspace(KEYSPACE)
+        final String keyspace = properties.getProperty("spring.data.cassandra.keyspace-name", "test");
+        final SchemaStatement statement = SchemaBuilder.createKeyspace(keyspace)
                 .ifNotExists()
                 .with()
                 .replication(ImmutableMap.of("class", "SimpleStrategy", "replication_factor", REPLICATION_FACTOR));
         getSession().execute(statement);
-        session.execute(String.format("USE %s;", KEYSPACE));
+        session.execute(String.format("USE %s;", keyspace));
     }
 
     private void createTables() {
@@ -105,9 +115,12 @@ public final class CassandraConnector {
     }
 
     private static void insertWeakPasswords() {
-        final InputStream inputStream = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream(DEFAULT_WEAK_PASSWORD_FILE);
-        PasswordFileUtil.uploadWeakPasswords(inputStream);
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(DEFAULT_WEAK_PASSWORD_FILE)) {
+            PasswordFileUtil.uploadWeakPasswords(inputStream);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     private static void insertDefaultUser() {
